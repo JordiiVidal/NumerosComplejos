4bc593156b715c1b27167ebc3d56fc2838491258
Jordi Vidal <jordividal1997@gmail.com>
2018-11-21 13:13:38 +0100

    magic setters and getters

--- complejo.php
+++ complejo.php
@@ -6,53 +6,44 @@ class Complejo{
     private $imaginaria;
 
     public function __construct(float $real, float $imaginaria){
-        $this->setReal($real);
-        $this->setImaginaria($imaginaria);
+        $this->real = $real;
+        $this->imaginaria = $imaginaria;
     }
 
     //getters
-    function getReal(){
-        return $this->real;
-    }
-    function getImaginaria(){
-        return $this->imaginaria;
+    function __get($propiedad){
+
+        return $this->propiedad;
+
     }
 
+
     //setters
-    function setReal($real){
-        if(empty($real)){
-            throw new Exception('Numero real Vacio');
-        }else{
-            $this->real = $real;
-        }
-    }
-    function setImaginaria($imaginaria){
-        if(empty($imaginaria)){
-            throw new Exception('Numero imaginario Vacio');
-        }else{
-            $this->imaginaria = $imaginaria;
-        }
+    function __set($propiedad, $valor){
+
+        $this->$propiedad = $valor;
+
     }
 
     //functions 
     function sumar(Complejo $complejo){
-        $this->setReal($this->getReal() + $complejo->getReal());
-        $this->setImaginaria($this->getImaginaria() + $complejo->getImaginaria());
+        $this->real = $this->real + $complejo->real;
+        $this->imaginaria = $this->imaginaria + $complejo->imaginaria;
     }
 
     function mutiplicar(Complejo $complejo){
-        $this->setReal($this->getReal() * $complejo->getReal());
-        $this->setImaginaria($this->getImaginaria() * $complejo->getImaginaria());
+        $this->real = $this->real * $complejo->real;
+        $this->imaginaria = $this->imaginaria * $complejo->imaginaria;
     }
     function igual(Complejo $complejo){
-        if(($this->getReal() == $complejo->getReal()) && ($this->getImaginaria() == $complejo->getImaginaria())){
+        if(($this->real == $complejo->real) && ($this->imaginaria == $complejo->imaginaria)){
             return true;
         }
         return false;
     }
 
     function toString(){
-        return '('.$this->getReal().' , '.$this->getImaginaria().')';
+        return '('.$this->real.' , '.$this->imaginaria.')';
     }
 
 }
